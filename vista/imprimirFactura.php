3c2ba848ece77363b4487e56f51ed36a88550340
deboch <dschamorro_90@hotmail.com>
2020-01-13 16:10:08 -0300

    Estilo factura

--- vista/imprimirFactura.php
+++ vista/imprimirFactura.php
@@ -10,8 +10,14 @@ include_once("../head.php");
 document.getElementById("cliente").innerHTML = document.getElementById("cliente").value;
 document.getElementById("telefono").innerHTML = document.getElementById("telefono").value;
 document.getElementById("fecha").innerHTML = document.getElementById("fecha").value;
-
+	
 }
+function calcularBonificacion(elemento){
+	var bonificacion = elemento.value;
+	var conDescuento = document.getElementById("conDescuento").value;
+	conDescuento = conDescuento*(bonificacion/100);
+	
+	}
 </script>
 <body>
 
@@ -143,23 +149,75 @@ document.getElementById("fecha").innerHTML = document.getElementById("fecha").va
 				<br>
 				
 				</table>
-					<h2>Observaciones:</h2>
-					<h3>Bonificacion Global (%)</h3>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="separador"></div>
+					</div>
+				</div>
+				<div class="row">
+				<div class="col-md-8">
+					<h4>Observaciones:</h4>
+					<h5>Bonificacion Global (%)</h5>
 					<input type="text" name="bonificacion" id="bonificacion" value="" onchange="calcularBonificacion('bonificaciion');">
+				</div>
+				<div class="col-md-4">
+					
 					<?php
-                        echo "<h2>SubTotal:</h2>	
-							  <h2>".$total."</h2>
-							  <input type='text' name='total' id='total' value= '".$total."' hidden='hidden'>
-					 		  <h2>Total Bonificado:</h2>
-							  <input type='text' name='conDescuento' id='conDescuento'>";	
+						echo "<div class='row'>
+									<div class='col-md-8' >
+										<h4 style='float:right;'>SubTotal: $</h4>
+									</div>
+									<div class='col-md-4'>	
+										<h4>".$total."</h4><input class='form-control' type='text' name='total' id='total' value= '".$total."' hidden='hidden'>
+									</div>
+								</div>
+								<div class='row'>
+									<div class='col-md-8'>
+										<h4 style='float:right;'>Total Bonificado: $</h4>
+									</div>
+									<div class='col-md-4'>
+										<input class='form-control' type='text' name='conDescuento' id='conDescuento'>	
+									</div>
+								</div>"	;
 					?>	
 
-					<p>Forma de pago: Cheque pago diferido 7 días fecha factura.
-					Transcurrido dicho plazo, se ajustará de acuerdo al valor del dólar estaunidense, tipo
-					vendedor Banco Nación del día anterior al del efectivo pago.-</p>
-
-					<p>Forma de pago : 
-					Plazo de entrega :</p>
+				</div>
+				</div>
+					<br><br>
+					<div class="row">
+						<div class="col-md-12">
+						<div class="separador"></div>
+						</div>
+					</div>
+					<br>
+				<div class="FormaDePago">
+					<div class="row">
+						<h5>Forma de pago: Cheque pago diferido 7 días fecha factura.
+						Transcurrido dicho plazo, se ajustará de acuerdo al valor del dólar estaunidense, tipo
+						vendedor Banco Nación del día anterior al del efectivo pago.-</h5>
+					</div>
+					<br>
+					<div class="row">
+					<div class="col-md-3">
+						<h5>Forma de pago :</h5> 
+					</div>
+					<div class="col-md-3">
+						<select class="form-control" name="formaDePago">
+								<option value="">Efectivo</option>
+								<option value="">Cheque</option>
+								<option value="">Tarjeta de crédito</option>
+								<option value="">Tarjeta de débito</option>
+						</select>
+					</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<h5>Plazo de entrega:</h5>
+						</div>
+						<div class="col-md-3">
+							<input class="form-control" type="number" min="0">
+						</div>
+					</div>
 					
 				<br><br><br><br><br><br>
                 <button class="btn btn-success float-right agregar"><a href="javascript:crearPdf()" style="color:white!important">Descargar</a></button>
@@ -192,4 +250,4 @@ document.getElementById("fecha").innerHTML = document.getElementById("fecha").va
     }
 </script>
 
-        </html>
+</html>
